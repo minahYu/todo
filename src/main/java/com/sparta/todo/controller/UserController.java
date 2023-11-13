66bfa009613a81dfabbdb1a890144582273aa6d3
minahYu <mina010311@naver.com>
2023-11-14 01:38:56 +0900

    Implement: 회원가입 기능 구현

--- src/main/java/com/sparta/todo/controller/UserController.java
+++ src/main/java/com/sparta/todo/controller/UserController.java
@@ -2,10 +2,21 @@ package com.sparta.todo.controller;
 
 import com.sparta.todo.dto.SignupRequestDto;
 import com.sparta.todo.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+@Slf4j
 @Controller
 @RequestMapping("/api/auth")
 public class UserController {
@@ -16,8 +27,19 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public void signup(SignupRequestDto requestDto) {
-        //return userService.signup(requestDto);
+    public ResponseEntity signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
+        ResponseEntity responseEntity = null;
+        if(bindingResult.hasErrors()) {
+            List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
+            for(FieldError fieldError : fieldErrorList) {
+                log.error(fieldError.getField());
+                responseEntity = new ResponseEntity(fieldError.getDefaultMessage(), HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            userService.signup(requestDto);
+            responseEntity = new ResponseEntity("회원가입을 완료하였습니다.", HttpStatus.OK);
+        }
+        return responseEntity;
     }
 
     @PostMapping("/login")
--- src/main/java/com/sparta/todo/service/UserService.java
+++ src/main/java/com/sparta/todo/service/UserService.java
@@ -1,11 +1,36 @@
 package com.sparta.todo.service;
 
 import com.sparta.todo.dto.SignupRequestDto;
+import com.sparta.todo.entity.User;
+import com.sparta.todo.repository.UserRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UserService {
-    public void signup(SignupRequestDto requestDto) {
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void signup(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+        // DB에 중복된 username이 있는지 확인
+        Optional<User> usernameCheck = userRepository.findByUsername(username);
+        System.out.println(usernameCheck);
+        if(usernameCheck.isPresent()) {
+            // orElseThrow()?
+            throw new IllegalArgumentException("중복된 username이 있습니다.");
+        }
+
+        // 회원 저장
+        // But 작업 중 오류 발생할 경우 모든 작업 취소(rollback)
+        User user = new User(username, password);
+        System.out.println("Password : " + requestDto.getPassword());
+        userRepository.save(user);
     }
 }
